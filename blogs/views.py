80fb1d2d89d291acea60890b44ccd8183ba0a605
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-12-09 14:12:37 -0600

    navbar functionality

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.utils import timezone
+from django.db.models import Q
 
 from blogs.models import Post, Category
 
@@ -36,3 +37,55 @@ class PostDetailView(generic.DetailView):
         context['categories'] = Category.objects.all()
         return context
 
+
+class FeaturedListView(generic.ListView):
+    model = Post
+    template_name = 'blogs/results.html'
+
+    def get_queryset(self):
+        query = Post.objects.filter(featured=True).filter(
+            pub_date__lte=timezone.now()
+        )
+        return query
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class CategoryListView(generic.ListView):
+    model = Post
+    template_name = 'blogs/results.html'
+
+    def get_queryset(self):
+        query = self.request.path.replace('/category/', '')
+        print(query)
+        post_list = Post.objects.filter(categories__slug=query).filter(
+            pub_date__lte=timezone.now()
+        )
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class SearchResultsView(generic.ListView):
+    model = Post
+    template_name = 'blogs/results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        post_list = Post.objects.filter(
+            Q(title__icontains=query) | Q(categories__title__icontains=query)
+        ).filter(
+            pub_date__lte=timezone.now()
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
