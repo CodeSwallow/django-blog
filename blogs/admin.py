51ad1f1c85e162c7b2b603d98a681ed35dfc550b
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-12-16 14:14:24 -0600

    comment model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from blogs.models import Post, Category
+from blogs.models import Post, Category, Comment
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Comment)
--- blogs/models.py
+++ blogs/models.py
@@ -40,3 +40,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=1000, help_text='Ingrese un comentario')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-post_date']
+
+    def __str__(self):
+        len_title = 15
+        if len(self.content) > len_title:
+            return self.content[:len_title] + '...'
+        return self.content
